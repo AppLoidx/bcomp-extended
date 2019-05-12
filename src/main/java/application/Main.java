60c04224f922a7cc23818a7a8c0f3bce9de9f2ee
Arthur Desk <apploidyakutsk@gmail.com>
2019-05-12 20:47:09 +0300

    renamed main-class file from Nightmare to Main

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -12,7 +12,7 @@ import ru.ifmo.cs.bcomp.ui.MPDecoder;
  * @author Arthur Kupriyanov
  *
  */
-public class Nightmare {
+public class Main {
     public static void main(String[] args) throws Exception {
         String mpname;
         String app;
